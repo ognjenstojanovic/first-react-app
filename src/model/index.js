737a17c464e7b7a5053a563dd790f9a66b6e08ce
stefan <stefan.bakoc@eurocom-group.eu>
2018-06-18 15:17:17 +0200

    Add counter example

--- src/model/index.js
+++ src/model/index.js
@@ -3,8 +3,39 @@ export const initialState = {
 }
 
 
+export const types = {
+    INCREMENT: 'counter/INCREMENT',
+    DECREMENT: 'counter/DECREMENT',
+}
+
+
+export const actions = {
+    increment: (amount) => {
+        return {
+            type: types.INCREMENT,
+            payload: amount
+        }
+    },
+    decrement: () => {
+        return {
+            type: types.DECREMENT
+        }
+    },
+}
+
+
 export const reducer = function (state = initialState, action) {
     switch(action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
         default:
             return state
     }
